7c30224831ac8d78c638e94cdcf4e919ce4eac77
Jan Krüger <jkrueger@cebitec.uni-bielefeld.de>
2023-01-11 15:27:24 +0100

    add configuration (file) support adjust documentation

--- denbi/vendordata/__init__.py
+++ denbi/vendordata/__init__.py
@@ -1,4 +1,13 @@
+"""
+Module that uses the Openstack API to create project specific user data (elixir id, elixir_name, ssh keys, ...).
+"""
+
+import logging
 import os_client_config
+import os.path
+import sys
+import yaml
+from yaml.loader import SafeLoader
 
 # cloud="openstack-dev"
 #
@@ -13,3 +22,40 @@ import os_client_config
 
 identity = os_client_config.make_rest_client("identity")
 sdk = os_client_config.make_sdk()
+log = logging.getLogger("nova_dynamic_vendordata")
+
+def check_configuration():
+    """ Check validity of given configuration."""
+    if config["cache"]:
+        log.debug(f"Found option 'cache' {config['cache']}")
+        if isinstance(config["cache"],int):
+            log.error("Option 'cache' must be of type int.")
+            return False
+    else:
+        config["cache"] = 300
+    if config["cloud"]:
+        log.debug("Found option cloud.")
+    for units in ("domains","projects"):
+        if config[units]:
+            log.debug("Found option domains.")
+            for listtype in ("blocklist","allowlist"):
+                if config[units][listtype] and isinstance(config[units][listtype],list):
+                    log.debug(f"Found option {units}->{listtype} ({','.join(config[units][listtype])})")
+                else:
+                    log.error(f"Option {units}->{listtype} is must be of type list.")
+                    return False
+    return True
+
+
+# check if configuration file available
+if os.path.isfile("/etc/nova_dynamic_vendordata.yaml"):
+    try:
+        with open('/etc/nova_dynamic_vendordata.yaml',encoding="UTF-8") as f:
+            config = yaml.load(f, Loader=SafeLoader)
+            if not check_configuration():
+                sys.exit(1)
+    except OSError as e:
+        log.error(e)
+        sys.exit(1)
+else:
+    config = {'cache':300}
--- denbi/vendordata/vendordata.py
+++ denbi/vendordata/vendordata.py
@@ -1,21 +1,36 @@
-import logging
+"""
+Module that uses the Openstack API to create project specific user data (elixir id, elixir_name, ssh keys, ...).
+"""
 
 from flask import Flask, request
-from denbi.vendordata import identity, sdk
+from denbi.vendordata import identity, sdk, config, log
 
 app = Flask(__name__)
 
-LOG = logging.getLogger()
+
 
 
 @app.post("/")
 def vendordata():
+    """ Return a list of users (with metadata) for given project id.
+        If project_id is block listed or not in allow listed an empty
+        json document is returned.
+    """
     data = request.get_json()
 
     if "project-id" in data:
-        id = data["project-id"]
-        # check for blacklisted project
-        return __userlist_by_project(project_id=id)
+        project_id = data["project-id"]
+
+        # allowlist is set and project id is not in allowlist
+        if "allowlist" in config and project_id not in config["allowlist"]:
+            log.info(f"Project id {project_id} is not in allowlist.")
+            return None
+        # blocklist is set and project id is in blocklist
+        if "blocklist" in config and project_id in config["blocklist"]:
+            log.info(f"Project id {project_id} is in blocklist.")
+            return None
+
+        return __userlist_by_project(project_id=project_id)
 
     return None
 
@@ -37,7 +52,7 @@ def __userlist_by_project(project_id):
         identity_prefix = "s3/"
 
     # get a list of all users, luckily also non-standard metadata ist returned when using REST-API
-    _userlist = dict()
+    _userlist = {}
     for _user in identity.get(f"{identity_prefix}users").json()["users"]:
         # id and name are always present
         _tmp = {"id": _user["id"],
@@ -58,11 +73,11 @@ def __userlist_by_project(project_id):
         if "user" in _role_assignment: # we are only interested in users not groups or roles
             _userset.add(_role_assignment["user"]["id"])
 
-    result = list()
+    result = []
     for _user_id in _userset:
         _tmp = _userlist[_user_id]
         # get list of key pairs for _user_id
-        _kp_list = list()
+        _kp_list = []
         for _kp in sdk.list_keypairs({"user_id": _user_id}):
             if _kp.type == "ssh":
                 _kp_list.append(_kp.public_key)
